97ec92398b04e6666b128b9bb064d7b28aed3535
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-11-10 21:35:02 +0900

    [Update] main, headerfileを修正

--- srcs/main.c
+++ srcs/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tashimiz <tashimiz@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 20:55:13 by tashimiz          #+#    #+#             */
+/*   Updated: 2022/11/10 20:55:15 by tashimiz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 int	main(int argc, char **argv)
@@ -9,31 +21,8 @@ int	main(int argc, char **argv)
 	controller = init_push_swap(argc, argv);
 	receive_cmdline_argument(controller);
 	sort_and_check_duplication(controller);
-
-	print_array(controller->original_array, controller->argc - 1);
-	puts("");
-	print_array(controller->sorted_array, controller->argc - 1);
-	puts("");
-
-	/* dstack_print(controller->stack_a); */
-	/* dstack_print(controller->stack_b); */
-	/* puts(""); */
-
-	/* print_array(controller->sorted_array, controller->argc - 1); */
-	/* puts(""); */
-
 	compress(controller);
-	dstack_print(controller->stack_a);
-	/* dstack_print(controller->stack_b); */
-	puts("");
-
-	/* sort_lte_three(controller); */
-	/* sort_lte_five(controller); */
-	dstack_print(controller->stack_a);
-	puts("");
-
-	ops_print(controller->ops);
-	if (!is_stack_sorted(controller->stack_a) || 12 < controller->ops->size)
-		exit(EXIT_FAILURE);
+	sort_main(controller);
+	ops_print_result(controller);
 	exit(EXIT_SUCCESS);
 }
