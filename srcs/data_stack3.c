390c64510af086e6e98c666ee59e8d5ad49ad9ef
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-11-10 21:06:41 +0900

    [Add/Fix] stackの中から値を検索する関数の追加と、最小値を検索してくる関数のバグを修正

--- srcs/data_stack3.c
+++ srcs/data_stack3.c
@@ -1,51 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_helper.c                                      :+:      :+:    :+:   */
+/*   data_stack3.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tashimiz <tashimiz@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/10 20:58:07 by tashimiz          #+#    #+#             */
-/*   Updated: 2022/11/10 20:58:09 by tashimiz         ###   ########.fr       */
+/*   Created: 2022/11/10 20:58:17 by tashimiz          #+#    #+#             */
+/*   Updated: 2022/11/10 20:58:18 by tashimiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// find the maximum value on given stack
-size_t	get_max(t_dstack *stack)
+bool	dstack_find(t_dstack *stack, size_t target)
 {
 	t_dnode	*nil;
 	t_dnode	*current;
-	size_t	max;
 
 	nil = stack->nil;
 	current = nil->next;
-	max = 0;
 	while (current != NULL && current != nil)
 	{
-		if (max < current->num)
-			max = current->num;
+		if (current->num == target)
+			return (true);
 		current = current->next;
 	}
-	return (max);
-}
-
-// find the minimum value on given stack
-size_t	get_min(t_dstack *stack)
-{
-	t_dnode	*nil;
-	t_dnode	*current;
-	size_t	min;
-
-	nil = stack->nil;
-	current = nil->next;
-	min = SIZE_T_MAX;
-	while (current != NULL && current != nil)
-	{
-		if (current->num < min)
-			min = current->num;
-		current = current->next;
-	}
-	return (min);
+	return (false);
 }
